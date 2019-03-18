7a7ae7b9cdd437ec98cac470bec92c37e099c139
廖敏男 <minnan0328@gmail.com>
2019-03-18 18:39:58 +0800

    update

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -21,47 +21,49 @@
       </div>
       <div id="other">
         <div class="content_box">
-          <div class="title">
-            <h1 class="">BM25</h1>
-            <h1 class="">BM25</h1>
-            <h1 class="">BM25</h1>
-            <h1 class="">BM25</h1>
+          <div class="content_title">
+            <h1 class="">BM21</h1>
+            <h1 class="">BM22</h1>
+            <h1 class="">BM23</h1>
+            <h1 class="">BM24</h1>
           </div>
           <div class="content">
-            <div class="but_box lefe">
-              <button><</button>
+            <div class="but_box left">
+              <button @click="left()"><</button>
             </div>
-            <div class="content_text">
-              <h1 class="ContentTitle">標題</h1>
-              <div class="date">2019-02-15</div>
-              <p class="text">
-                內文內文
-                <span>內文內文</span>內文內文
-              </p>
-            </div>
-            <div class="content_text">
-              <h1 class="ContentTitle">標題</h1>
-              <div class="date">2019-02-15</div>
-              <p class="text">
-                內文內文
-                <span>內文內文</span>內文內文
-              </p>
-            </div>
-            <div class="content_text">
-              <h1 class="ContentTitle">標題</h1>
-              <div class="date">2019-02-15</div>
-              <p class="text">
-                內文內文
-                <span>內文內文</span>內文內文
-              </p>
-            </div>
-            <div class="content_text">
-              <h1 class="ContentTitle">標題</h1>
-              <div class="date">2019-02-15</div>
-              <p class="text">
-                內文內文
-                <span>內文內文</span>內文內文
-              </p>
+            <div class="move">
+              <div class="content_text">
+                <h1 class="ContentTitle">標題</h1>
+                <div class="date">2019-02-15</div>
+                <p class="text">
+                  內文內文
+                  <span>內文內文</span>內文內文
+                </p>
+              </div>
+              <div class="content_text">
+                <h1 class="ContentTitle">標題</h1>
+                <div class="date">2019-02-15</div>
+                <p class="text">
+                  內文內文
+                  <span>內文內文</span>內文內文
+                </p>
+              </div>
+              <div class="content_text">
+                <h1 class="ContentTitle">標題</h1>
+                <div class="date">2019-02-15</div>
+                <p class="text">
+                  內文內文
+                  <span>內文內文</span>內文內文
+                </p>
+              </div>
+              <div class="content_text">
+                <h1 class="ContentTitle">標題</h1>
+                <div class="date">2019-02-15</div>
+                <p class="text">
+                  內文內文
+                  <span>內文內文</span>內文內文
+                </p>
+              </div>
             </div>
             <div class="but_box right">
               <button @click="right()">></button>
@@ -79,15 +81,43 @@ export default {
   name: "Search",
   data() {
     return {
-      search:''
+      search:'',
+      isRight: true,
+      isLeft: false,
+      nub: 0
     };
   },
   mounted () {
     this.$store.dispatch('postAdmin')
   },
   methods:{
-    right() {}
-
+    right() {
+      var box = document.getElementsByClassName("move")[0];
+      var title = document.getElementsByClassName("content_title")[0];
+      if(this.isRight === true){
+        box.style.left = box.offsetLeft - 552 + "px";
+        title.style.left = title.offsetLeft - 552 + "px";
+        this.isLeft = true
+        this.nub = this.nub +1
+        if(this.nub === 3){
+          this.isRight = false
+        }
+      }
+    },
+    left() {
+      var box = document.getElementsByClassName("move")[0];
+      var title = document.getElementsByClassName("content_title")[0];
+      if(this.isLeft === true){
+        box.style.left = box.offsetLeft + 400 + "px";
+        title.style.left = title.offsetLeft + 400 + "px";
+        this.nub = this.nub - 1
+        if(this.nub === 0){
+          this.isLeft = false
+          this.isRight = true
+        }
+      }
+      console.log(box.offsetLeft,title.offsetLeft)
+    }
   }
 };
 </script>
